d88f2a86e30f4dfc54c8074b2c42d2ded10c8e68
Ren-0-Ren-0 <samyakratna.bajracharya2062@gmail.com>
2025-10-29 17:27:08 +0545

    made route

--- 2025_java_OOP/src/main/java/com/mycompany/mavenproject1/route.java
+++ 2025_java_OOP/src/main/java/com/mycompany/mavenproject1/route.java
@@ -8,6 +8,25 @@ package com.mycompany.mavenproject1;
  *
  * @author poema
  */
+
+import java.util.Scanner;
+
 public class route {
     
+    private String[] stops;
+    
+    public void makeRoute(String... cities){
+        this.stops=cities;
+    }
+    
+    public static void main(String[] args) {
+        System.out.println("list the cities of this route ");
+        Scanner take_route= new Scanner(System.in);
+        String input = take_route.nextLine();
+        String[] parts = input.split("[,\\s]+");
+        route r=new route();
+        r.makeRoute(parts);
+        
+    }
 }
+    
