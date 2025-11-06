af5af18cc59cabae360bdc7e38d356cd94c8f630
Ren-0-Ren-0 <samyakratna.bajracharya2062@gmail.com>
2025-11-06 09:26:50 +0545

    class stuff

---
+++ 2025_java_OOP/src/main/java/com/mycompany/mavenproject1/Person.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author poema
+ */
+public class Person{
+    
+    //instance variable/field
+    private String name="";
+    
+    //class variable
+    private static int personCount=0;
+    
+    //constructor
+    public Person(String Name){
+        name=Name;
+        personCount++;
+    }
+    
+    //getter
+    public String getName(){
+        return name;
+    }
+    
+    //instance method
+    public void introduce(){
+        System.out.println("Hello, I am " + this.name);
+    }
+    
+    //class method
+    public static void population(){
+        System.out.println("There are " + personCount +" people here.");
+    }
+}
---
+++ 2025_java_OOP/src/main/java/com/mycompany/mavenproject1/Student.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author poema
+ */
+public class Student extends Person {
+    private String StudentID;
+    private static int num_of_student=0;
+    
+    public Student(String name, String ID){
+        super(name);
+        this.StudentID=ID;
+    }
+    
+    @Override
+    public void introduce(){
+        System.out.println("Hello, I am a Student. My name is "+ this.getName() + "and my student ID is "+ this.StudentID);
+    }
+}
