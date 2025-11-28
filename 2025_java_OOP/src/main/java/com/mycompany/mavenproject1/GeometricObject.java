64332f695a39e8074f8669148fed4f78fd6b227b
Ren-0-Ren-0 <samyakratna.bajracharya2062@gmail.com>
2025-11-28 09:20:24 +0545

    Update GeometricObject.java

--- 2025_java_OOP/src/main/java/com/mycompany/mavenproject1/GeometricObject.java
+++ 2025_java_OOP/src/main/java/com/mycompany/mavenproject1/GeometricObject.java
@@ -8,6 +8,48 @@ package com.mycompany.mavenproject1;
  *
  * @author poema
  */
-public class GeometricObject {
+public abstract class GeometricObject {
+    private boolean filled;
+    private String color;
+    
+    public GeometricObject(){};
+    
+    public GeometricObject(String Color, boolean Fill){
+        
+        this.color = Color;
+        this.filled = Fill;
+        
+    };
+    
+    public String getColor(){
+        return color;
+    }
+    
+    public void setColor(String Color){
+        this.color=Color;
+    }
+    
+    public boolean isFilled(){
+        return filled;
+    }
+    
+    public void setFilled(boolean fill){
+        this.filled=fill;
+    }
+    
+    
+    abstract double getPerimeter();
+    
+    abstract double getArea();
+    
+    abstract String getShape();
+    
+    @Override
+    public String toString(){
+        String print="this geometric object is of color: " + color + "and is ";
+        if(filled==true){print = print + "filled";}else if(filled==false){print = print + "not filled";}
+        else{print="ERROR";}
+        return print;
+    }
     
 }
