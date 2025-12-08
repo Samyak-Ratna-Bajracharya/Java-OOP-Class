c7c630fb0e1c6235a083bed3775b5600a5a39203
Ren-0-Ren-0 <samyakratna.bajracharya2062@gmail.com>
2025-12-08 10:27:54 +0545

    Create CustomException.java

---
+++ 2025_java_OOP/src/main/java/com/mycompany/mavenproject1/CustomException.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Exception.java to edit this template
+ */
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author poema
+ */
+public class CustomException {
+
+    /**
+     * Creates a new instance of <code>CustomException</code> without detail
+     * message.
+     */
+    public CustomException() {
+    }
+
+    /**
+     * Constructs an instance of <code>CustomException</code> with the specified
+     * detail message.
+     *
+     * @param msg the detail message.
+     */
+    public CustomException(String msg) {
+        super(msg);
+    }
+}
